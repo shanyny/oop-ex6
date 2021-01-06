387aa6be0ab178302e3b7255af73af8977e08fee
shanygindi <shanygindi@gmail.com>
2021-01-06 23:58:25 +0200

    Testing git...

--- variables/Variable.java
+++ variables/Variable.java
@@ -4,6 +4,7 @@ import java.util.function.Predicate;
 
 public class Variable {
     protected final String name;
+    protected boolean DeleteMeeee;
     protected Object value;
     private boolean isInitialized = false;
     private final boolean isFinal;
