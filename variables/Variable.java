1b8a2d2a3f90d5ba6aa26e33d09e482bfcd094f1
shanygindi <shanygindi@gmail.com>
2021-01-07 00:01:57 +0200

    Test was good!

--- variables/Variable.java
+++ variables/Variable.java
@@ -4,7 +4,6 @@ import java.util.function.Predicate;
 
 public class Variable {
     protected final String name;
-    protected boolean DeleteMeeee;
     protected Object value;
     private boolean isInitialized = false;
     private final boolean isFinal;
