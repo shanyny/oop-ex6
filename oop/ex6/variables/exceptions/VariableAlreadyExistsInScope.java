8a7eb487f1eaf107e10c33c72740218153de7c6a
royur <roy.urbach@mail.huji.ac.il>
2021-01-08 11:28:37 +0200

    changed to more informative exception

---
+++ oop/ex6/variables/exceptions/VariableAlreadyExistsInScope.java
@@ -0,0 +1,9 @@
+package oop.ex6.variables.exceptions;
+
+/**
+ * This exception is thrown if a variable in current scope is being declared for the second time.
+ * @author Shany Gindi and Roy Urbach
+ */
+public class VariableAlreadyExistsInScope extends VariableException {
+    private static final long serialVersionUID = 1L;
+}
--- oop/ex6/variables/exceptions/VariableAlreadyInitialized.java
+++
@@ -1,9 +0,0 @@
-package oop.ex6.variables.exceptions;
-
-/**
- * This exception is thrown if a variable in current scope is being initialized for the second time.
- * @author Shany Gindi and Roy Urbach
- */
-public class VariableAlreadyInitialized extends VariableException {
-    private static final long serialVersionUID = 1L;
-}
