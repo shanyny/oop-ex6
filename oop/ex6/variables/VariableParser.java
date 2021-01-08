26a6ac8f658754bae5d384aa8c13ba6fe3a2b9ed
royur <roy.urbach@mail.huji.ac.il>
2021-01-08 13:57:03 +0200

    fixed some global variables problems and added documentation.

--- oop/ex6/variables/VariableParser.java
+++ oop/ex6/variables/VariableParser.java
@@ -7,83 +7,150 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * This class.
+ * This class is a abstract class with static methods that parse a line which initializes or sets a variable.
+ * @author Shany Gindi and Roy Urbach
  */
-public class VariableParser {
+public abstract class VariableParser {
 
     private static final String FINAL = "final";
-
     private static final String VALUE_FORMAT = "[\\w\"'\\-\\. ]+";
-    private static final String NAME_FORMAT = "[a-zA-Z|_][\\w]*";
-    private static final String FORMAT =
-            String.format("^(%s +)?([a-zA-Z]+) +((?:%s *(?:=? *%s)?, *)*" +
-                    "(?:%s *(?:=? *%s)?)) *;$", FINAL, NAME_FORMAT, VALUE_FORMAT, NAME_FORMAT, VALUE_FORMAT);
-    private static final Pattern PATTERN = Pattern.compile(FORMAT);
-
-
+    private static final String NAME_FORMAT = "(?:[a-zA-Z]|(?:_\\w))\\w*";
     private static final String SINGLE_VARIABLE_REGEX =
             String.format("(%s) *(?:= *(%s))?",NAME_FORMAT, VALUE_FORMAT);
-
-    private static final Pattern SINGLE_VARIABLE_PATTERN =
-            Pattern.compile(SINGLE_VARIABLE_REGEX);
-
+    private static final String FORMAT =
+            String.format("^(%s +)?([a-zA-Z]+) +((?:%s, *)*(?:%s)) *;$",
+                    FINAL, SINGLE_VARIABLE_REGEX, SINGLE_VARIABLE_REGEX);
     private static final String SET_VARIABLE_REGEX =
             String.format("^ *(%s) *= *(%s) *;$",  NAME_FORMAT, VALUE_FORMAT);
+
+    private static final Pattern SINGLE_VARIABLE_PATTERN = Pattern.compile(SINGLE_VARIABLE_REGEX);
+    private static final Pattern FULL_PATTERN = Pattern.compile(FORMAT);
     private static final Pattern SET_VARIABLE_PATTERN = Pattern.compile(SET_VARIABLE_REGEX);
 
+    /**
+     * This method receives a Block and a line (string), and updates the block's variable archives,
+     * while checking for errors.
+     * @param block - the block to update the variables of.
+     * @param line - the line to parse.
+     * @throws InvalidVariableInitialization - if the line is not in format.
+     * @throws TypeNotFoundException - if the given type is not valid.
+     * @throws NewValueNotCompatible - if the value given to a variable is not compatible with its type.
+     * @throws VariableIsFinal - if trying to assign a value to a final variable.
+     * @throws VariableDoesntExist - if trying to assign a variable that doesn't exist to a variable.
+     * @throws VariableNotInitialized - if trying to assign a not-initialized variable to a variable.
+     * @throws FinalVariableNotInitialized - if trying to declare a final variable without initializing it.
+     * @throws VariableAlreadyExistsInScope - if declaring a variable that already exists in scope.
+     */
     public static void parseVariableLine(Block block, String line)
             throws InvalidVariableInitialization, TypeNotFoundException, NewValueNotCompatible,
-            IllegalVariableName, VariableIsFinal, VariableDoesntExist, VariableNotInitialized,
-            FinalVariableNotInitialized, VariableAlreadyInitialized {
-        Matcher initializingMatcher = PATTERN.matcher(line);
+            VariableIsFinal, VariableDoesntExist, VariableNotInitialized,
+            FinalVariableNotInitialized, VariableAlreadyExistsInScope {
+
+        Matcher initializingMatcher = FULL_PATTERN.matcher(line);
         Matcher setVariableMatcher = SET_VARIABLE_PATTERN.matcher(line);
+
+        // if an initialization line
         if (initializingMatcher.find()) initializeVariablesParser(block, initializingMatcher);
-        else if (setVariableMatcher.find()) setVariable(block, setVariableMatcher);
+
+        // if assigning line
+        else if (setVariableMatcher.find()) assignVariable(block, setVariableMatcher);
+
+        // else error
         else throw new InvalidVariableInitialization();
     }
 
+    /**
+     * This method initializes variables and adds them to the given block.
+     * @param block - the block to add the variables to.
+     * @param matcher - the matcher that holds the variable's strings.
+     * @throws TypeNotFoundException - if the given type is not valid.
+     * @throws NewValueNotCompatible - if the value given to a variable is not compatible with its type.
+     * @throws VariableIsFinal - if trying to assign a value to a final variable.
+     * @throws VariableNotInitialized - if trying to assign a not-initialized variable to a variable.
+     * @throws FinalVariableNotInitialized - if trying to declare a final variable without initializing it.
+     * @throws VariableAlreadyExistsInScope - if declaring a variable that already exists in scope.
+     */
     private static void initializeVariablesParser(Block block, Matcher matcher)
             throws TypeNotFoundException, NewValueNotCompatible, VariableIsFinal,
-            IllegalVariableName, VariableNotInitialized, FinalVariableNotInitialized, VariableAlreadyInitialized {
-        boolean isFinal = matcher.group(1) != null;
-        VariableType variableType = VariableType.getType(matcher.group(2));
-        String str = matcher.group(3);
-        Matcher singleVariableMatcher = SINGLE_VARIABLE_PATTERN.matcher(str);
-        while (singleVariableMatcher.find()) {
-            String varStr = singleVariableMatcher.group(1);
-            if (block.getVariable(varStr, true) != null) throw new VariableAlreadyInitialized();
-            String valueStr = singleVariableMatcher.group(2);
-            Variable valueVar = block.getVariable(valueStr, false);
-            if (valueVar == null) createVariable(block, varStr, variableType, valueStr, isFinal);
-            else createVariable(block, varStr, variableType, valueVar, isFinal);
+            VariableNotInitialized, FinalVariableNotInitialized, VariableAlreadyExistsInScope {
+        boolean isFinal = matcher.group(1) != null;  // are the variables declared final
+        VariableType variableType = VariableType.getType(matcher.group(2));  // the variables' type.
+
+        String variablesString = matcher.group(3);
+        Matcher singleVariableMatcher = SINGLE_VARIABLE_PATTERN.matcher(variablesString);
+        while (singleVariableMatcher.find()) {  // initialize all variables in the line
+            String varName = singleVariableMatcher.group(1);  // the name of the variable
+
+            // if the variable already exists in scope and is not global
+            Variable variable = block.getVariable(varName, true);
+            if (variable != null && !variable.isGlobal()) throw new VariableAlreadyExistsInScope();
+
+            String valueStr = singleVariableMatcher.group(2);  // the value of the variable to assign
+
+            createVariable(block, varName, variableType, valueStr, isFinal, false);
         }
     }
 
-    private static void setVariable(Block block, Matcher matcher)
+    /**
+     * This method assigns a new value to a variable.
+     * @param block - the block to add the variables to.
+     * @param matcher - the matcher that holds the variable's strings.
+     * @throws TypeNotFoundException - if the given type is not valid.
+     * @throws NewValueNotCompatible - if the value given to a variable is not compatible with its type.
+     * @throws VariableIsFinal - if trying to assign a value to a final variable.
+     * @throws VariableNotInitialized - if trying to assign a not-initialized variable to a variable.
+     * @throws FinalVariableNotInitialized - if trying to declare a final variable without initializing it.
+     * @throws VariableDoesntExist - if trying to assign a variable that doesn't exist to a variable.
+     */
+    private static void assignVariable(Block block, Matcher matcher)
             throws VariableDoesntExist, VariableIsFinal, NewValueNotCompatible, VariableNotInitialized,
-            IllegalVariableName, TypeNotFoundException, FinalVariableNotInitialized {
-        String varStr = matcher.group(1);
+            TypeNotFoundException, FinalVariableNotInitialized {
+        String varName = matcher.group(1);
+
+        Variable var = block.getVariable(varName, false);
+        if (var == null) throw new VariableDoesntExist();  // if the variable isn't in any parent scope
+
         String valueStr = matcher.group(2);
-        Variable var = block.getVariable(varStr, false);
-        Variable valVar = block.getVariable(valueStr, false) ;
-        if (var == null) throw new VariableDoesntExist();
-        if (var.isGlobal() && !block.isGlobal() && !var.isInitialized()) {
-            if (valVar != null) createVariable(block, var.getName(), var.getType(), valVar, false);
-            else createVariable(block, var.getName(), var.getType(), valueStr, false);
-        } else if (valVar != null) var.setValue(valVar);
+
+        // if the variable is global and the scope isn't, create local global variable
+        if (var.isGlobal() && !block.isGlobal()) {
+            createVariable(block, var.getName(), var.getType(), valueStr, false, true);
+        }
+
+        // set the value
+        Variable valueVar = block.getVariable(valueStr, false);
+        if (valueVar != null) var.setValue(valueVar);
         else var.setValue(valueStr);
     }
 
-    private static void createVariable(Block block, String name, VariableType type, Variable value,
-                                       boolean isFinal)
-            throws NewValueNotCompatible, IllegalVariableName, VariableIsFinal, TypeNotFoundException,
-            VariableNotInitialized, FinalVariableNotInitialized {
-        block.addVariable(new Variable(name, type, value,isFinal, block.isGlobal()));
-    }
+    /**
+     * This method creates a new variable and adds it to the block given.
+     * @param block - the block to add the variable to.
+     * @param name - the name for the new variable.
+     * @param type - the type of the new variable.
+     * @param value - the value of the new variable (null if not initialized). Can be a variable's name.
+     * @param isFinal - is the variable final.
+     * @param isGlobal - if the variable global.
+     * @throws NewValueNotCompatible - if the value given to a variable is not compatible with its type.
+     * @throws VariableIsFinal - if trying to assign a value to a final variable.
+     * @throws TypeNotFoundException - if the given type is not valid.
+     * @throws FinalVariableNotInitialized - if trying to declare a final variable without initializing it.
+     * @throws VariableNotInitialized - if trying to assign a not-initialized variable to a variable.
+     */
+    private static void createVariable(Block block, String name, VariableType type, String value,
+                                       boolean isFinal, boolean isGlobal)
+            throws NewValueNotCompatible, VariableIsFinal,
+            TypeNotFoundException, FinalVariableNotInitialized, VariableNotInitialized {
 
-    private static void createVariable(Block block, String name, VariableType type, String value, boolean isFinal)
-            throws NewValueNotCompatible, IllegalVariableName, VariableIsFinal,
-            TypeNotFoundException, FinalVariableNotInitialized {
-        block.addVariable(new Variable(name, type, value,isFinal, block.isGlobal()));
+        if (value != null) {
+            Variable valueVar = block.getVariable(value, false);
+            if (valueVar != null) {
+                // if the value is a variable, assign it as value for the new variable
+                block.addVariable(new Variable(name, type, valueVar, isFinal, isGlobal));
+                return;
+            }
+        }
+        // if not, create the new variable with the string given
+        block.addVariable(new Variable(name, type, value, isFinal, block.isGlobal()));
     }
 }
